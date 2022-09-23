c24ed1966c95a66f54fae1c259dd8566599d2100
LuchoDelArco <47138072@est.ort.edu.ar>
2022-09-23 10:34:15 -0300

    ClassFinished

---
+++ Assets/BarreraABEScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarreraABEScript : MonoBehaviour
+{
+	[SerializeField] Animator Anim;
+
+	void OnTriggerEnter()
+	{
+		Anim.SetBool("ObjetoEnRango", true);
+	}
+	
+	void OnTriggerExit()
+	{
+		Anim.SetBool("ObjetoEnRango", false);
+	}
+}
